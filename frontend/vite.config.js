22d79e725e2b92fa2ff7e452e0a4f7bee5950668
nikhilreddy <akhilreddygundla2@gmail.com>
2026-03-23 15:39:54 +0530

    Added deployed backend url in config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://paytm-hacks.vercel.app',
         changeOrigin: true,
       }
     }
